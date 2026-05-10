8097803b3a700706a93b643d970a80cfecab9b11
Victor Salmon <victorsalmon@hotmail.com>
2026-05-10 14:13:11 -0700

    feat: add Github pages functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://epicrope.com',
+  site: 'https://the-great-jaoman.github.io',
+  base: '/epicrope-site',
   vite: {
     plugins: [tailwindcss()]
   }
